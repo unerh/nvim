0ffe113497a7ee1dc9dfd5c3cbc8a63bd32ea629
Hakan Uner <ubersoft.au@gmail.com>
2022-02-14 22:12:57 +1100

    use kanagawa. set gruvbox beforehand to get better lazygit highlighting

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,12 @@
+local default_colors = require("kanagawa.colors").setup()
+
 vim.cmd[[
 try
-  colorscheme darkplus
+  colorscheme gruvbox
+  colorscheme kanagawa
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
 endtry
 ]]
+
