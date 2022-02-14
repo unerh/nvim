c4520f318a6d3478bba8e3b9fef010c280a910e1
Hakan Uner <ubersoft.au@gmail.com>
2022-02-14 22:29:25 +1100

    test out sneak for now..

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -55,6 +55,7 @@ return packer.startup(function(use)
   use 'goolord/alpha-nvim'
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
+  use "justinmk/vim-sneak"
 
   use {"ellisonleao/glow.nvim", config = 'vim.g.glow_width = 125'}
 
