046793689b3bf476f45807cce5e46ec89c2242ca
Hakan Uner <ubersoft.au@gmail.com>
2022-02-14 22:14:38 +1100

    shifting made suitable for colemak layout

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -36,8 +36,8 @@ keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "<A-e>", "<Esc>:m .+1<CR>==gi<ESC>", opts)
+keymap("n", "<A-i>", "<Esc>:m .-2<CR>==gi<ESC>", opts)
 
 -- Insert --
 -- Press jk fast to enter
@@ -49,16 +49,16 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "<A-e>", ":m .+1<CR>==", opts)
+keymap("v", "<A-i>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-e>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-i>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
