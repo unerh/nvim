c5abdd0dbc3cb16c51f8378f3ff2ed96dd7c6818
Hakan Uner <ubersoft.au@gmail.com>
2022-02-24 09:55:02 +1100

    make gruvbox great again (thanks to primeagen)

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,26 @@
-local default_colors = require("kanagawa.colors").setup()
+-- local default_colors = require("kanagawa.colors").setup()
 
 vim.cmd[[
 try
+  let g:gruvbox_contrast_dark = 'medium'
+  if exists('+termguicolors')
+      let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+      let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+  endif
+  let g:gruvbox_invert_selection='0'
+  let g:gruvbox_italic=1
+
+  highlight ColorColumn ctermbg=0 guibg=grey
+  hi SignColumn guibg=none
+  hi CursorLineNR guibg=None
+  highlight Normal guibg=none
+  " highlight LineNr guifg=#ff8659
+  " highlight LineNr guifg=#aed75f
+  highlight LineNr guifg=#5eacd3
+  highlight netrwDir guifg=#5eacd3
+  highlight qfFileName guifg=#aed75f
+  hi TelescopeBorder guifg=#5eacd
+
   colorscheme gruvbox
   colorscheme kanagawa
 catch /^Vim\%((\a\+)\)\=:E185/
