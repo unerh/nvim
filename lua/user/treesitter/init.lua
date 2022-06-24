043dd49b46b34d872bbbdbad52794b495e9bd9c0
Hakan Uner <ubersoft.au@gmail.com>
2022-06-24 11:33:16 +1000

    need to add languages explicitly now

--- lua/user/treesitter/init.lua
+++ lua/user/treesitter/init.lua
@@ -1,21 +1,41 @@
-
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
 	return
 end
 
-configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-    disable = { "" },  -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    }
-}
+configs.setup({
+	-- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = {
+		"python",
+		"go",
+		"typescript",
+		"javascript",
+		"rust",
+		"yaml",
+		"json",
+		"bash",
+		"nix",
+		"lua",
+		"make",
+		"dockerfile",
+		"c",
+		"cpp",
+		"html",
+		"css",
+		"java",
+		"comment",
+    "markdown"
+	},
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = true,
+	},
+	indent = { enable = true, disable = { "yaml" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+})
